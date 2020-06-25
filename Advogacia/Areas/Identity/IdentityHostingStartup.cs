1d2be1d6b16231b1e62bf2b5fa4e620d860139db
AnaBrando <anapchavesweb@gmail.com>
2020-06-25 15:50:08 -0300

    go

---
+++ Advogacia/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Advogacia.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Advogacia.Areas.Identity.IdentityHostingStartup))]
+namespace Advogacia.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Advogacia/Models/Artigo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Advogacia.Models
+{
+    public class Artigo
+    {
+        public long Id { get; set; }
+
+        public DateTime Data { get; set; }
+        public byte[] PhotoFile { get; set; }
+        public string ImageName { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
